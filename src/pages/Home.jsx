19fea1ebc25aa1d9bddd8e57ab900a6c196d4981
BeckieMorton <beckam11@gmail.com>
2023-10-17 14:28:31 +0200

    team apple exercises week 9 olga bec mirela

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,39 @@
+// - This component displays a list of the "Rolling Stones 500 Greatest Songs of all Time".
+// - Songs data is imported from a JSON file and is mapped to display each song with a link.
+// - The link for each song is dynamically generated based on the song's title. This will be crucial when setting up dynamic routes.
+
 // TODO: Import necessary modules and data
+import songs from "../data/songs.json";
+import { Link } from "react-router-dom";
+import SongInfo from "./SongInfo";
+
 function Home() {
   // TODO: Implement logic to display the list of songs
+
   return (
     <>
       <div>
         <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
-        {/* TODO: Display the list of songs with links */}
+        {songs.songs.map((song) => (
+          <Link to={`/song/${song.title}`} key={song.rank}>
+            <p>{song.title}</p>
+            {/* <SongInfo
+              name={songs.title}
+              artist={songs.artist}
+              rank={songs.rank}
+              album={songs.album}
+              year={songs.year}
+            /> */}
+          </Link>
+        ))}
       </div>
     </>
   );
+
+  // {burgers.map((burger) => (
+  //   <Link to={`/burgers/${burger.slug}`} key={burger.name}>
+  //     <Burger name={burger.name} image={burger.image} />
+  //   </Link>
 }
 
 export default Home;
--- src/pages/SongInfo.jsx
+++ src/pages/SongInfo.jsx
@@ -1,14 +1,41 @@
+// - This component is responsible for displaying detailed information about a specific song.
+// - It uses the `useParams` hook from `react-router-dom` to fetch the song title from the URL.
+// - Based on the song title from the URL, it finds the corresponding song from the imported JSON data.
+// - If the song is found, it displays its title, artist, album, and year. If not, it displays a "Song not found!" message.
+
 // TODO: Import necessary modules and data
+import songs from "../data/songs.json";
+import { useParams } from "react-router-dom";
 
 // TODO: Destrcutrue props to receive information coming in from the parent comp
 function SongInfo() {
   // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
 
+  const { songTitle } = useParams();
+  const songMatch = songs.songs.find((p) => p.title === songTitle);
+
+  console.log(songMatch);
+
+  return (
+    <div>
+      <h1> Song information</h1>
+      <p>Title: {songMatch.title}</p>
+      <p>Artist: {songMatch.artist}</p>
+      <p>Rank: {songMatch.rank}</p>
+      <p>Album: {songMatch.album}</p>
+      <p>Year: {songMatch.year}</p>
+    </div>
+  );
+
   return (
     <>
       <div>
         <h2>Song Information</h2>
+        <p>rank is: {songs.rank}</p>
         {/* TODO: Display song details or a "Song not found!" message */}
+        {songs.songs.map((song) => (
+          <p>{song.title}</p>
+        ))}
       </div>
     </>
   );
